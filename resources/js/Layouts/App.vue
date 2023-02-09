6a739a165c6d3b7ca8ba2b399e332cf020f75797
Mahmoud Shiref <sh1r3f@gmail.com>
2023-02-09 21:38:55 +0200

    wip

--- resources/js/Layouts/App.vue
+++ resources/js/Layouts/App.vue
@@ -7,6 +7,12 @@ import Footer from './Footer.vue';
 const props = defineProps({
     flash: Object,
 });
+if (props.flash.message) {
+    this.toastr.success(this.$page.props.flash.message);
+}
+if (props.flash.error) {
+    this.toastr.error(this.$page.props.flash.error);
+}
 console.log(props);
 </script>
 
@@ -15,8 +21,6 @@ console.log(props);
         <title>{{ __('Home') }}</title>
         <!-- Flash messages -->
         <component is="script" defer v-if="route()?.params?.verified == 1"> this.toastr.success('{{ __('Email has been verified successfully') }}') </component>
-        <component is="script" defer v-if="this.$page.props.flash.message"> this.toastr.success(this.$page.props.flash.message); </component>
-        <component is="script" defer v-if="this.$page.props.flash.error"> this.toastr.error(this.$page.props.flash.error); </component>
     </Head>
 
     <!-- Mini bar -->
