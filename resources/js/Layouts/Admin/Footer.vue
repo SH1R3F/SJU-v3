2e6020d73e9742998800a36005ec06a8d6128fc6
Mahmoud Shiref <sh1r3f@gmail.com>
2023-02-10 15:45:43 +0200

    Remove my footer copyrights

--- resources/js/Layouts/Admin/Footer.vue
+++ resources/js/Layouts/Admin/Footer.vue
@@ -2,11 +2,7 @@
 <template>
     <footer class="content-footer footer bg-footer-theme">
         <div class="container-xxl">
-            <div class="footer-container d-flex align-items-center justify-content-between py-2 flex-md-row flex-column">
-                <div>
-                    © 2023 , {{ __('made with ❤️ by') }}
-                    <a class="text text-link" href="https://github.com/sh1r3f">Sh1r3f</a>
-                </div>
+            <div class="footer-container d-flex align-items-center justify-content-end py-1 flex-md-row flex-column">
                 <div>
                     جميع الحقوق محفوظة
                     <a class="text text-link" :href="route('home')">هيئة الصحفيين السعوديين</a>
