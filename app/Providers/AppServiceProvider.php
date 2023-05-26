0279ae13e7341c8919a0921e66a75048e092ded0
Mahmoud Shiref <Sh1r3f@gmail.com>
2023-05-26 16:17:08 +0300

    ⚙️ chore(provider): Force https scheme for URL's

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -36,6 +36,9 @@ class AppServiceProvider extends ServiceProvider
         Notification::extend('sms', function () {
             return new SmsChannel;
         });
+
+        \URL::forceScheme('https');
+
         // Schema::defaultStringLength(125);
         // $this->app->bind('path.public', function () {
         //     return base_path() . '/../public_html/v2.sju.org.sa';
