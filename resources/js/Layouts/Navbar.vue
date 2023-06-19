e016c02a48f49c8ebe4cf4efa3112bfdfe2354a1
Mahmoud Shiref <sh1r3f@gmail.com>
2023-06-19 21:13:19 +0300

    🛠️refactor(layout): remove commented code and add training bag link to navbar

--- resources/js/Layouts/Navbar.vue
+++ resources/js/Layouts/Navbar.vue
@@ -3,15 +3,8 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <button
-                class="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarTogglerDemo03"
-                aria-controls="navbarTogglerDemo03"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            >
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03"
+                aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
@@ -41,8 +34,11 @@
                     <li class="nav-item mx-2">
                         <Link href="#" class="nav-link">{{ __('Radio & TV') }}</Link>
                     </li>
-                    <li class="nav-item mx-2">
+                    <!-- <li class="nav-item mx-2">
                         <Link href="#" class="nav-link">{{ __('Partnership') }}</Link>
+                    </li> -->
+                    <li class="nav-item mx-2">
+                        <Link :href="route('pages.training-bag')" class="nav-link">{{ __('Training bag') }}</Link>
                     </li>
                 </ul>
             </div>
